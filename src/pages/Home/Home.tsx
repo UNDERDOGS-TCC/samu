633a0fd51cb2cd13c30337701bb5c61f04083b13
ouwargui <guikemon2@gmail.com>
2022-03-24 02:50:55 -0300

    chore: move home page to its own folder

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,20 @@
+import {useNavigation} from '@react-navigation/native';
+import React from 'react';
+import {Button} from 'react-native';
+import {Container, Text} from './styles';
+
+const Home: React.FC = () => {
+  const navigation = useNavigation();
+
+  return (
+    <Container>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <Button
+        title="Configurações"
+        onPress={() => navigation.navigate('Config' as never)}
+      />
+    </Container>
+  );
+};
+
+export default Home;
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -3,11 +3,11 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  background-color: ${(props) => props.theme.colors.background};
+  background-color: ${(props) => props.theme.main.colors.background};
 `;
 
 export const Text = styled.Text`
-  color: ${(props) => props.theme.colors.text};
+  color: ${(props) => props.theme.main.colors.text};
 `;
 
 export const style = StyleSheet.create({
