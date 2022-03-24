b265ab2d7ac4000574210ee91b226b59b5294fcf
ouwargui <guikemon2@gmail.com>
2022-03-24 02:51:29 -0300

    feat: add new route to config screen

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import Home from '../pages/Home';
+import Home from '../pages/Home/Home';
 import {useTheme} from '../themes/ThemeManagerProvider';
 import {darkMode, lightMode} from '../themes/theme';
+import Config from '../pages/Config/Config';
 
 const Stack = createStackNavigator();
 
@@ -14,6 +15,7 @@ const Routes: React.FC = () => {
     <NavigationContainer theme={theme.isDarkMode ? darkMode : lightMode}>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Config" component={Config} />
       </Stack.Navigator>
     </NavigationContainer>
   );
