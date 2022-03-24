1112a590645634ee5f6021350141083abbfd7483
ouwargui <guikemon2@gmail.com>
2022-03-24 02:51:08 -0300

    feat: load new fonts

--- App.tsx
+++ App.tsx
@@ -1,12 +1,41 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import {
+  useFonts,
+  Ubuntu_300Light,
+  Ubuntu_300Light_Italic,
+  Ubuntu_400Regular,
+  Ubuntu_400Regular_Italic,
+  Ubuntu_500Medium,
+  Ubuntu_500Medium_Italic,
+  Ubuntu_700Bold,
+  Ubuntu_700Bold_Italic,
+} from '@expo-google-fonts/ubuntu';
+import AppLoading from 'expo-app-loading';
 import Routes from './src/routes/Routes';
 import ThemeManagerProvider from './src/themes/ThemeManagerProvider';
 
-const App: React.FC = () => (
-  <ThemeManagerProvider>
-    <Routes />
-  </ThemeManagerProvider>
-);
+const App: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    Ubuntu_300Light,
+    Ubuntu_300Light_Italic,
+    Ubuntu_400Regular,
+    Ubuntu_400Regular_Italic,
+    Ubuntu_500Medium,
+    Ubuntu_500Medium_Italic,
+    Ubuntu_700Bold,
+    Ubuntu_700Bold_Italic,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <ThemeManagerProvider>
+      <Routes />
+    </ThemeManagerProvider>
+  );
+};
 
 export default App;
