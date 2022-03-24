414488003c0a42477b622469ee884c81936ecaea
ouwargui <guikemon2@gmail.com>
2022-03-24 02:52:11 -0300

    feat: separate navigation colors from env colors and add some new css vars

--- src/themes/theme.ts
+++ src/themes/theme.ts
@@ -1,29 +1,77 @@
 export const lightMode = {
   dark: false,
   colors: {
-    primary: '#18A0FB',
-    secondary: '#1C3253',
-    lightblue: '#18A0FB',
-    darkblue: '#1C3253',
     background: '#FFFFFF',
+    border: '#d8d8d8',
     card: '#FFFFFF',
+    notification: 'rgb(255, 59, 48)',
+    primary: '#000000',
     text: '#000000',
-    border: '#303030',
-    notification: '#18A0FB',
+  },
+  main: {
+    colors: {
+      primary: '#18A0FB',
+      secondary: '#1C3253',
+      lightblue: '#18A0FB',
+      darkblue: '#1C3253',
+      background: '#FFFFFF',
+      text: '#000000',
+    },
+    font: {
+      family: {
+        ubuntu: {
+          regular: 'Ubuntu_400Regular',
+          medium: 'Ubuntu_500Medium',
+          bold: 'Ubuntu_700Bold',
+        },
+      },
+      size: {
+        small: '14px',
+        regular: '18px',
+        big: '22px',
+      },
+      weight: {
+        bold: 'bold',
+      },
+    },
   },
 };
 
 export const darkMode = {
   dark: true,
   colors: {
-    primary: '#1C3253',
-    secondary: '#18A0FB',
-    lightblue: '#18A0FB',
-    darkblue: '#1C3253',
     background: '#303030',
+    border: '#171717',
     card: '#303030',
+    notification: 'rgb(255, 69, 58)',
+    primary: '#FFFFFF',
     text: '#FFFFFF',
-    border: '#303030',
-    notification: '#1C3253',
+  },
+  main: {
+    colors: {
+      primary: '#1C3253',
+      secondary: '#18A0FB',
+      lightblue: '#18A0FB',
+      darkblue: '#1C3253',
+      background: '#303030',
+      text: '#FFFFFF',
+    },
+    font: {
+      family: {
+        ubuntu: {
+          regular: 'Ubuntu_400Regular',
+          medium: 'Ubuntu_500Medium',
+          bold: 'Ubuntu_700Bold',
+        },
+      },
+      size: {
+        small: '14px',
+        regular: '18px',
+        big: '22px',
+      },
+      weight: {
+        bold: 'bold',
+      },
+    },
   },
 };
