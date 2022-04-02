a2a74e8afa19bbfa9afe7d78ef7f7a1a629dc6ac
ouwargui <guikemon2@gmail.com>
2022-04-02 16:12:11 -0300

    feat: add icon to header left of drawer

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,7 +1,7 @@
-import {View, Text} from 'react-native';
 import React from 'react';
+import {View, Text} from 'react-native';
 
-const Profile = () => (
+const Profile: React.FC = () => (
   <View>
     <Text>Profile</Text>
   </View>
