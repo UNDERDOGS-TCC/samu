339082aa7316e568b03f46ae4b3646b35585cdfd
ouwargui <guikemon2@gmail.com>
2022-04-11 23:20:23 -0300

    feat: change sidebar route name

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -28,7 +28,7 @@ const Routes: React.FC = () => {
         }}
       >
         <Stack.Screen
-          name="Home"
+          name="Sidebar"
           component={Sidebar}
           options={{headerShown: false}}
         />
